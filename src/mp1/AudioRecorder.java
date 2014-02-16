6dcd818e32633b0165edd36448c82e3e17cbda56
Elvin <edtubillara@gmail.com>
2014-02-16 16:42:14 -0600

    did a simple audio recorder

---
+++ src/mp1/AudioRecorder.java
@@ -0,0 +1,32 @@
+package mp1;
+
+import org.gstreamer.ElementFactory;
+import org.gstreamer.Gst;
+import org.gstreamer.Pipeline;
+import org.gstreamer.Element;
+import org.gstreamer.State;
+import org.gstreamer.elements.FileSink;
+
+import common.GStreamerLoader;
+
+public class AudioRecorder {
+	private static Pipeline pipe;
+	public static void main(String[] args) {
+
+		GStreamerLoader.loadGStreamer();
+		args = Gst.init("AudioRecorder", args);
+		pipe = new Pipeline("pipeline");
+		final Element mic = ElementFactory.make("osxaudiosrc", "mic");
+		final Element audioresample =  ElementFactory.make("audioresample", "audioresample");
+		final Element audioconvert =  ElementFactory.make("audioconvert", "audioconvert");
+		final Element encoder = ElementFactory.make("wavenc", "encoder");
+		FileSink fileSink = new FileSink("fileSink");
+		fileSink.setLocation("/Users/masterjildo/Downloads/audiothing.wav");
+		pipe.addMany(mic,audioresample,audioconvert,encoder,fileSink);
+		Element.linkMany(mic,audioresample,audioconvert, encoder,fileSink);
+		pipe.setState(State.PLAYING);
+		Gst.main();
+		pipe.setState(State.NULL);
+	}
+
+}
