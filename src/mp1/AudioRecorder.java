9246af79474c8829fa30a6025115b4d6bd31496f
Son Nguyen <son@wirelessprvnat-172-16-251-138.near.illinois.edu>
2014-02-17 19:42:20 -0600

    src/mp1/AudioRecorder.java

--- src/mp1/AudioRecorder.java
+++ src/mp1/AudioRecorder.java
@@ -1,5 +1,6 @@
 package mp1;
 
+import org.gstreamer.Caps;
 import org.gstreamer.ElementFactory;
 import org.gstreamer.Gst;
 import org.gstreamer.Pipeline;
@@ -10,23 +11,83 @@ import org.gstreamer.elements.FileSink;
 import common.GStreamerLoader;
 
 public class AudioRecorder {
-	private static Pipeline pipe;
-	public static void main(String[] args) {
-
-		GStreamerLoader.loadGStreamer();
-		args = Gst.init("AudioRecorder", args);
+	
+	private Pipeline pipe;
+	/**
+	 * Opens up an audio pipeline from recorder to file
+	 */
+	
+	private Element mic;
+	private Element audioresample;
+	private Element audioconvert;
+	private Element encoder;
+	private Element caps;
+	private String format;
+	private String samplingRate;
+	private String sampleSize;
+	private String numberOfChannels;
+	FileSink fileSink;
+	
+	public void setFormat(String format){
+		this.format = format;
+	}
+	public void setSamplingRate(String samplingRate){
+		this.samplingRate=samplingRate;
+	}
+	public void setSampleSize(String sampleSize){
+		this.sampleSize=sampleSize;
+	}
+	public void setNumberOfChannels(String numberOfChannels){
+		this.numberOfChannels=numberOfChannels;
+	}
+	
+	public AudioRecorder(String micName, String audioresampleName, String audioconvertName, String encoderName, String location){
+		this.mic = ElementFactory.make(micName, "mic");
+		this.audioresample =  ElementFactory.make(audioresampleName, "audioresample");
+		this.audioconvert =  ElementFactory.make(audioconvertName, "audioconvert");
+		this.encoder = ElementFactory.make(encoderName, "encoder");
+		fileSink = new FileSink("fileSink");
+		fileSink.setLocation(location);
+	}
+	
+	public AudioRecorder(String location){
+		mic = ElementFactory.make("osxaudiosrc", "mic");
+		audioresample =  ElementFactory.make("audioresample", "audioresample");
+		audioconvert =  ElementFactory.make("audioconvert", "audioconvert");
+		encoder = ElementFactory.make("wavenc", "encoder");
+		fileSink = new FileSink("fileSink");
+		fileSink.setLocation(location);
+	}
+	
+	public void setCaps(){
+		this.caps = ElementFactory.make("capsfilter", "caps");
+		
+		String capsString = this.format+",rate="+this.samplingRate+",channels="+this.numberOfChannels+",width="+this.audioresample+",depth="+this.audioresample;;
+		this.caps.setCaps(Caps.fromString(capsString));
+		
+	}
+	
+	private void run(){
 		pipe = new Pipeline("pipeline");
-		final Element mic = ElementFactory.make("osxaudiosrc", "mic");
-		final Element audioresample =  ElementFactory.make("audioresample", "audioresample");
-		final Element audioconvert =  ElementFactory.make("audioconvert", "audioconvert");
-		final Element encoder = ElementFactory.make("wavenc", "encoder");
-		FileSink fileSink = new FileSink("fileSink");
-		fileSink.setLocation("/Users/son/Downloads/audiothing.wav");
+		this.setCaps();
 		pipe.addMany(mic,audioresample,audioconvert,encoder,fileSink);
 		Element.linkMany(mic,audioresample,audioconvert, encoder,fileSink);
-		pipe.setState(State.PLAYING);
+		
+		pipe.setState(State.PLAYING);		
 		Gst.main();
 		pipe.setState(State.NULL);
+	}
+	
+	
+	public static void main(String[] args) {
+		GStreamerLoader.loadGStreamer();
+		args = Gst.init("AudioRecorder", args);
+		AudioRecorder ar = new AudioRecorder("/Users/masterjildo/Desktop/audiothing.wav");
+		ar.setFormat("audio-alaw");
+		ar.setNumberOfChannels("1");
+		ar.setSampleSize("16");
+		ar.setSamplingRate("44100");
+		ar.run();
 	}
 
 }
